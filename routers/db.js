9593261d56efff00edd4ff980c23657b9897c0dc
Antoinette Belebele <anthobelebele@gmail.com>
2024-07-18 09:55:27 +0100

    add a file dotenv

--- routers/db.js
+++ routers/db.js
@@ -1,18 +1,26 @@
 const { Pool } = require('pg');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const pool = new Pool({
     user: process.env.DATA_USERNAME,
-    mot_de_passe: process.env.DATA_MOT_DE_PASSE , 
-    hôte : process.env.DATA_HOTE , 
+    passsword: process.env.DATA_MOT_DE_PASSE , 
+    host : process.env.DATA_HOTE , 
     port : process.env.DATA_PORT ,
-    database : process.env.DATA_DATABASE
+    database : process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-
-module.exports = {
-    query : (text, params) =>{
-        pool.query(text, params)
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Erreur de connexion à la base de données', err.stack);
     }
-}
+    console.log('Connexion réussie à la base de données');
+    release();
+});
+module.exports = { pool };
+
+module.exports.query = (text, params) => {
+    return pool.query(text, params);
+};
